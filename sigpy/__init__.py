35ab92fcd6f8a23cbcacb62502ef1c5f8efa5194
Zhengguo Tan <zhengguo.tan@gmail.com>
2023-05-09 22:25:11 +0200

    nlop in __init__

--- sigpy/__init__.py
+++ sigpy/__init__.py
@@ -9,7 +9,7 @@ that can be used in conjuction with Alg to form an App.
 
 """
 from .version import __version__ # noqa
-from sigpy import alg, app, config, linop, prox
+from sigpy import alg, app, config, linop, prox, nlop
 
 from sigpy import (backend, block, conv, interp,
                    fourier, pytorch, sim, thresh,
@@ -25,7 +25,7 @@ from sigpy.thresh import *  # noqa
 from sigpy.util import *  # noqa
 from sigpy.wavelet import *  # noqa
 
-__all__ = ['alg', 'app', 'config', 'linop', 'prox']
+__all__ = ['alg', 'app', 'config', 'linop', 'prox', 'nlop']
 __all__.extend(backend.__all__)
 __all__.extend(block.__all__)
 __all__.extend(conv.__all__)
